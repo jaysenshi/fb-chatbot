bff5e1ab4822599490354d44dfb6b18427df8a0e
Justin Donn <justin.donn@gmail.com>
2019-10-26 21:25:07 -0700

    takes into account multiple response messages now; also now requires at least one word as input

--- stats.py
+++ stats.py
@@ -13,19 +13,25 @@ import re
 import string
 from math import sqrt
 from random import randint
-import asyncio
-import websockets
+
+
 
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './Test1-7c40fa5b9a66.json'
 
 MAX_TIME_DELTA_MS = 4 * 60 * 60 * 1000
+MAX_CONSECUTIVE_TIME_DELTA_MS = 5 * 1000
 COMMON_WORDS_FILE = "100_most_common_english_words.json"
 
-if len(sys.argv) != 2:
-    print("usage: ./stats.py /path/to/message.json")
+if len(sys.argv) != 3:
+    print("usage: ./stats.py /path/to/message.json <local_debug_boolean>")
+    print("set <local_debug_boolean> to true for local debugging, anything else otherwise")
     sys.exit(0)
 
 data = json.loads(open(sys.argv[1]).read())
+LOCAL_DEBUG = True if sys.argv[2] == "true" or sys.argv[2] == "t" else False
+if not LOCAL_DEBUG:
+    import asyncio
+    import websockets
 
 # list of 100 most common english words from wordfrequency.info
 common_words = json.load(open(COMMON_WORDS_FILE))
@@ -41,7 +47,6 @@ for i in range(len(common_words)):
     score = sqrt(1 / ratio)
     common_words_score[word] = score
 # 'the' has a score of .13 while 'many' has a score of .99
-# print(common_words_score)
 
 msgs = data["messages"]
 msgs.reverse()
@@ -65,53 +70,10 @@ user = data["participants"][1]["name"]
 #     #print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 #     return sentiment.score, sentiment.magnitude
 
-# instead of numwords, use totalscore where each word is assigned a score based on "importance"
+# instead of numwords, use totalscore where each word is assigned a score based on "importance" (fixed?)
 # dictionary for converting i'm --> I'm --> im (fixed??)
 
 def reply(input):
-    words = input.split(" ")
-    best_num_words = 0
-    best_reply = []
-    for i in range(1,len(msgs)):
-        m = msgs[i]
-        if m.get("sender_name","") == user:
-            num_words = 0
-            for word in words:
-                # need to "normalize" the word and the message: all lowercase and get rid of punctuation
-                word_normalized = word.lower().translate(string.punctuation)
-                curr_content_normalized = m.get("content","").lower().translate(string.punctuation)
-                pattern = r"(^|[\W])"+word_normalized+r"($|[\W])" #todo: ignore special chars
-                if re.findall(pattern,curr_content_normalized):
-                    #print(re.findall(pattern,m.get("content","")))
-                    num_words+=1
-            if num_words>=best_num_words:
-                for j in range(len(msgs)-i):
-                    m2 = msgs[i+j]
-                    if m2.get("sender_name","") != user:
-                        # if the time between two messages is too long, the messages
-                        # are likely not realted anymore so we can just skip those cases
-                        if m2.get("timestamp_ms")-m.get("timestamp_ms")<=MAX_TIME_DELTA_MS:
-                            #print(m.get('content'))
-                            #todo: filter out "you set nickname", etc.
-                            if num_words>best_num_words:
-                                best_reply = []
-                            best_reply.append(m2.get("content"))
-                            break
-                best_num_words = num_words
-    if best_num_words>0:
-        reply = best_reply[randint(0,len(best_reply)-1)]
-        # print('Input sentiment: ' + str(get_sentiment(input)[0]))
-        # print(reply)
-        # return ('Reply sentiment: ' + str(get_sentiment(reply)[0]))
-        return reply
-    else:
-        while(True):
-            msg = msgs[randint(0,len(msgs)-1)]
-            if msg.get("sender_name",user) != user:
-                return "debug: not found"
-                #return msg.get("content")
-
-def reply2(input):
     words = input.split(" ")
     best_score = 0
     best_replies = []
@@ -122,23 +84,38 @@ def reply2(input):
             for word in words:
                 # need to "normalize" the word and the message: all lowercase and get rid of punctuation
                 word_normalized = word.lower().translate(string.punctuation)
+                # print(word_normalized)
+                if not word_normalized.isalnum():
+                    continue
                 curr_content_normalized = m.get("content","").lower().translate(string.punctuation)
                 pattern = r"(^|[\W])"+word_normalized+r"($|[\W])" #todo: ignore special chars
                 if re.findall(pattern,curr_content_normalized):
                     #print(re.findall(pattern,m.get("content","")))
                     score += common_words_score[word_normalized] if word_normalized in common_words_score.keys() else 1
-            if score>=best_score:
+            if score >= best_score:
                 for j in range(len(msgs)-i):
                     m2 = msgs[i+j]
                     if m2.get("sender_name","") != user:
                         # if the time between two messages is too long, the messages
                         # are likely not realted anymore so we can just skip those cases
-                        if m2.get("timestamp_ms")-m.get("timestamp_ms")<=MAX_TIME_DELTA_MS:
+                        if m2.get("timestamp_ms")-m.get("timestamp_ms") <= MAX_TIME_DELTA_MS:
                             #print(m.get('content'))
                             #todo: filter out "you set nickname", etc.
                             if score>best_score:
                                 best_replies = []
-                            best_replies.append(m2.get("content"))
+                            # get the message block that corresponds to this matched message
+                            k = 1
+                            next_msg = msgs[i + j + k]
+                            next_content = next_msg.get("content", "")
+                            msg_block = [m2.get("content", "")]
+                            while next_content and next_msg.get("sender_name", "") != user \
+                                and next_msg.get("timestamp_ms") - m2.get("timestamp_ms") < MAX_CONSECUTIVE_TIME_DELTA_MS:
+                                msg_block.append(next_content)
+                                k += 1
+                                m2 = next_msg
+                                next_msg = msgs[i + j + k]
+                                next_content = next_msg.get("content", "")
+                            best_replies.append(msg_block)
                             break
                 best_score = score
     if best_score>0:
@@ -149,16 +126,23 @@ def reply2(input):
         # print (best_score)
         return reply
     else:
-        while(True):
+        while True:
             msg = msgs[randint(0,len(msgs)-1)]
             if msg.get("sender_name",user) != user:
+                # todo: add way to tell user to include at least one word if we were given illegal input
                 return "debug: not found"
                 #return msg.get("content")
+
+if LOCAL_DEBUG:
+    while True:
+        print(reply(input()))
 
 async def chat(websocket, path):
     print("accepted client")
     async for msg in websocket:
-        await websocket.send(reply2(msg))
+        result = reply(msg)
+        for phrase in result:
+            await websocket.send(phrase)
 
 start_server = websockets.serve(chat, "127.0.0.1", 8080)
 
