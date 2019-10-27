33fb246b8a3e1d8bb6969b7dc2803b03b7886207
Justin Donn <justin.donn@gmail.com>
2019-10-26 19:11:15 -0700

    added weights to common words

--- stats.py
+++ stats.py
@@ -111,6 +111,49 @@ def reply(input):
                 return "debug: not found"
                 #return msg.get("content")
 
+def reply2(input):
+    words = input.split(" ")
+    best_score = 0
+    best_replies = []
+    for i in range(1,len(msgs)):
+        m = msgs[i]
+        if m.get("sender_name","") == user:
+            score = 0
+            for word in words:
+                # need to "normalize" the word and the message: all lowercase and get rid of punctuation
+                word_normalized = word.lower().translate(string.punctuation)
+                curr_content_normalized = m.get("content","").lower().translate(string.punctuation)
+                pattern = r"(^|[\W])"+word_normalized+r"($|[\W])" #todo: ignore special chars
+                if re.findall(pattern,curr_content_normalized):
+                    #print(re.findall(pattern,m.get("content","")))
+                    score += common_words_score[word_normalized] if word_normalized in common_words_score.keys() else 1
+            if score>=best_score:
+                for j in range(len(msgs)-i):
+                    m2 = msgs[i+j]
+                    if m2.get("sender_name","") != user:
+                        # if the time between two messages is too long, the messages
+                        # are likely not realted anymore so we can just skip those cases
+                        if m2.get("timestamp_ms")-m.get("timestamp_ms")<=MAX_TIME_DELTA_MS:
+                            #print(m.get('content'))
+                            #todo: filter out "you set nickname", etc.
+                            if score>best_score:
+                                best_replies = []
+                            best_replies.append(m2.get("content"))
+                            break
+                best_score = score
+    if best_score>0:
+        reply = best_replies[randint(0,len(best_replies)-1)]
+        # print('Input sentiment: ' + str(get_sentiment(input)[0]))
+        # print(reply)
+        # return ('Reply sentiment: ' + str(get_sentiment(reply)[0]))
+        # print (best_score)
+        return reply
+    else:
+        while(True):
+            msg = msgs[randint(0,len(msgs)-1)]
+            if msg.get("sender_name",user) != user:
+                return "debug: not found"
+                #return msg.get("content")
 while(True):
-    print(reply(input())) #TODO: serverify for tmrw
+    print(reply2(input())) #TODO: serverify for tmrw
 
